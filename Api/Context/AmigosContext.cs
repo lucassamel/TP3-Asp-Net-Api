2e01d037df370bf3e8df8ec6370f4add70271037
Lucas Samel <lucas.samel97@gmail.com>
2020-09-13 17:06:41 -0300

    Migration

---
+++ Api/Context/AmigosContext.cs
@@ -0,0 +1,36 @@
+﻿using Api.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Api.Context
+{
+    public class AmigosContext : DbContext
+    {
+        public AmigosContext(DbContextOptions<AmigosContext> options) : base(options)
+        {
+
+        }
+
+        public virtual DbSet<Amigo> Amigos { get; set; }
+
+        public class DesingTimeDbContextFactory : IDesignTimeDbContextFactory<AmigosContext>
+        {
+            public AmigosContext CreateDbContext(string[] args)
+            {
+                IConfigurationRoot configuration = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile(@Directory.GetCurrentDirectory() + "/appsettings.json").Build();
+
+                var builder = new DbContextOptionsBuilder<AmigosContext>();
+                var connectionString = configuration.GetConnectionString("DatabaseConnection");
+                builder.UseSqlServer(connectionString);
+
+                return new AmigosContext(builder.Options);
+            }
+        }
+    }
+}
---
+++ Api/Migrations/20200913200548_initial.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Api.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Amigos",
+                columns: table => new
+                {
+                    AmigoId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Sobrenome = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Telefone = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Aniversario = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Amigos", x => x.AmigoId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Amigos");
+        }
+    }
+}
